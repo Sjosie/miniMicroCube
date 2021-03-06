ea93ad66bcc4e71debe56c02df94d4a12fd7f1c5
sjosie <mipt.dppe@gmail.com>
2021-03-06 17:58:48 +0800

    Initial Commit

---
+++ miniMicroCube/miniMicroCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniMicroCubeApp.swift
+//  miniMicroCube
+//
+//  Created by Evgeny Protopopov on 06.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct miniMicroCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
