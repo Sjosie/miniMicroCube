b394e2cec0378ac9963791b5c05be87048a4f543
sjosie <mipt.dppe@gmail.com>
2021-03-07 14:30:07 +0800

    5 mins to go!

--- miniMicroCube/ContentView.swift
+++ miniMicroCube/ContentView.swift
@@ -4,13 +4,24 @@
 //
 //  Created by Evgeny Protopopov on 06.03.2021.
 //
+//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+
     var body: some View {
-        Text("Hello, world!")
+        Rectangle().fill(Color.green)
             .padding()
+            .background(Color.green)
+            .clipShape(Rectangle())
+            .frame(width: 100, height: 100, alignment: .center)
+            .scaleEffect(animationAmount)
+            .animation(.easeInOut(duration: 2))
+            .onAppear(perform: {
+                self.animationAmount += 1
+            })
     }
 }
 
